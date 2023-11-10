bc5fe62962cf6460f8b3ab79240c1e8957883977
dademon333 <dademon333@mail.ru>
2023-11-10 16:52:49 +0500

    Remove EnumDictSerializer

--- application/database/models/base.py
+++ application/database/models/base.py
@@ -1,24 +1,16 @@
-from enum import Enum
-
 from pydantic import BaseModel
 
 
-class EnumDictSerializer(BaseModel):
-    # Костыль для перевода enum items в int/str/etc при вызове model.dict()
-    def dict(self, **kwargs):
-        data = super().dict(**kwargs)
-        for field_name in data.keys():
-            if isinstance(data[field_name], Enum):
-                data[field_name] = data[field_name].value
-        return data
-
-
-class ModelBase(EnumDictSerializer):
+class ModelBase(BaseModel):
     id: int = 1  # default value will be ignored in create methods
 
     class Config:
         orm_mode = True
+        use_enum_values = True
 
 
-class UpdateModelBase(EnumDictSerializer):
+class UpdateModelBase(ModelBase):
     id: int
+
+    class Config:
+        use_enum_values = True
